577eda62ea6453fd1734340c3744c5fca2032c85
Danny Setiawan Syah <danns.6969@gmail.com>
2024-05-14 09:40:07 +0700

    rename update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Register from "./pages/register";
-import Login from "./pages/login";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import TaskDetail from "./pages/TaskDetail";
 
 function App() {
